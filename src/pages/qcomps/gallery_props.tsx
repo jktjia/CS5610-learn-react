cdbabeff7eba9e96923f6a2080dd3e6041aa9da3
jktjia <103521429+jktjia@users.noreply.github.com>
2024-09-26 17:14:54 -0400

    in class

--- src/pages/qcomps/gallery_props.tsx
+++ src/pages/qcomps/gallery_props.tsx
@@ -1,55 +1,55 @@
+import { GalleryProps } from "@/types/gallery";
+
+function Profile(props : GalleryProps) {
+  return (
+  <section className="profile">
+        <h2>{props.scientist.name}</h2>
+        <img
+          className="avatar"
+          src={'https://i.imgur.com/' + props.scientist.imageId + 's.jpg'}
+          alt={props.scientist.name}
+          width={70}
+          height={70}
+        />
+        <ul>
+          <li>
+            <b>Profession: </b>
+            {props.scientist.profession}
+          </li>
+          <li>
+            <b>Awards: {props.scientist.awards.split(',').length} </b>
+            {props.scientist.awards}
+          </li>
+          <li>
+            <b>Discovered: </b>
+            {props.scientist.discovery}
+          </li>
+        </ul>
+      </section>);
+}
+
 export default function Gallery() {
   return (
     <div>
       <h1>Notable Scientists</h1>
-      <section className="profile">
-        <h2>Maria Skłodowska-Curie</h2>
-        <img
-          className="avatar"
-          src='https://i.imgur.com/szV5sdGs.jpg'
-          alt="Maria Skłodowska-Curie"
-          width={70}
-          height={70}
+      <Profile
+        scientist={{
+          name: "Maria Skłodowska-Curie",
+          imageId: "szV5sdG",
+          profession: "physicist and chemist",
+          awards: "(Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)",
+          discovery: "polonium (element)"
+        }}
         />
-        <ul>
-          <li>
-            <b>Profession: </b>
-            physicist and chemist
-          </li>
-          <li>
-            <b>Awards: 4 </b>
-            (Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)
-          </li>
-          <li>
-            <b>Discovered: </b>
-            polonium (element)
-          </li>
-        </ul>
-      </section>
-      <section className="profile">
-        <h2>Katsuko Saruhashi</h2>
-        <img
-          className="avatar"
-          src='https://i.imgur.com/YfeOqp2s.jpg'
-          alt="Katsuko Saruhashi"
-          width={70}
-          height={70}
+        <Profile
+        scientist={{
+          name: "Katsuko Saruhashi",
+          imageId: "YfeOqp2",
+          profession: "geochemist",
+          awards: "(Miyake Prize for geochemistry, Tanaka Prize)",
+          discovery: "a method for measuring carbon dioxide in seawater"
+        }}
         />
-        <ul>
-          <li>
-            <b>Profession: </b>
-            geochemist
-          </li>
-          <li>
-            <b>Awards: 2 </b>
-            (Miyake Prize for geochemistry, Tanaka Prize)
-          </li>
-          <li>
-            <b>Discovered: </b>
-            a method for measuring carbon dioxide in seawater
-          </li>
-        </ul>
-      </section>
     </div>
   );
 }
